cab3f580409ec712575cdf0152556ca6945eca32
Yeshaya <andyshapiro613@gmail.com>
2024-10-20 22:12:04 -0400

    h

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
